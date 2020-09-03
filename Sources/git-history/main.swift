b6bb0a6298feefc0fd4abebd4ce7a8ea39dea211
Daniel Tull <dt@danieltull.co.uk>
2020-09-03 12:19:41 +0100

    Use GitKit to open the repository in the current working directory

--- Sources/git-history/main.swift
+++ Sources/git-history/main.swift
@@ -1,9 +1,17 @@
 
 import ArgumentParser
+import Foundation
+import GitKit
 
 struct GitHistory: ParsableCommand {
 
     func run() throws {
+        let path = FileManager.default.currentDirectoryPath
+        let url = URL(fileURLWithPath: path)
+        let repo = try Repository(url: url)
+        print(repo)
+        let commits = try repo.commits()
+        print(commits)
     }
 }
 
